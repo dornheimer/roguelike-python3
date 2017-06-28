ce3de347aaba1b2de64afce3071c26b0e045bb31
dornheimer <beaveroftime@gmail.com>
2017-06-28 23:14:22 +0200

    part 1

---
+++ input_handlers.py
@@ -0,0 +1,43 @@
+import libtcodpy as libtcod
+
+def handle_keys(key):
+    # Movement (arrow keys)
+    if key.vk == libtcod.KEY_UP:
+        return {'move': (0, -1)}
+    elif key.vk == libtcod.KEY_DOWN:
+        return {'move': (0, 1)}
+    elif key.vk == libtcod.KEY_LEFT:
+        return {'move': (-1, 0)}
+    elif key.vk == libtcod.KEY_RIGHT:
+        return {'move': (1, 0)}
+
+    # Movement (numpad)
+    elif key.vk == libtcod.KEY_KP1:
+        return {'move': (-1, 1)}
+    elif key.vk == libtcod.KEY_KP2:
+        return {'move': (0, 1)}
+    elif key.vk == libtcod.KEY_KP3:
+        return {'move': (1, 1)}
+    elif key.vk == libtcod.KEY_KP4:
+        return {'move': (-1, 0)}
+    elif key.vk == libtcod.KEY_KP5:
+        return {'move': (0, 0)}
+    elif key.vk == libtcod.KEY_KP6:
+        return {'move': (1, 0)}
+    elif key.vk == libtcod.KEY_KP7:
+        return {'move': (-1, -1)}
+    elif key.vk == libtcod.KEY_KP8:
+        return {'move': (0, -1)}
+    elif key.vk == libtcod.KEY_KP9:
+        return {'move': (1, -1)}
+
+    if key.vk == libtcod.KEY_ENTER and key.lalt:
+        # Alt+Enter: toggle full screen
+        return {'fullscreen': True}
+
+    elif key.vk == libtcod.KEY_ESCAPE:
+        # Exit the game
+        return {'exit': True}
+
+    # No key was pressed
+    return {}
