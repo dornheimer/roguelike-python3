3c81ee3fc641eaff6635eb06a428d36c237dca3f
dornheimer <beaveroftime@gmail.com>
2017-07-25 00:01:53 +0200

    completed part 7

--- death_functions.py
+++ death_functions.py
@@ -1,5 +1,6 @@
 import libtcodpy as libtcod
 
+from game_messages import Message
 from game_states import GameStates
 from render_functions import RenderOrder
 
@@ -11,14 +12,14 @@ def kill_player(player):
     player.char = '%'
     player.color = libtcod.dark_red
 
-    return 'You died!', GameStates.PLAYER_DEAD
+    return Message('You died!', libtcod.red), GameStates.PLAYER_DEAD
 
 
 def kill_monster(monster):
     """Monster death.
     Replace monster with corpse and reset its attributes.
     """
-    death_message = '{0} is dead!'.format(monster.name.capitalize())
+    death_message = Message('{0} is dead!'.format(monster.name.capitalize()), libtcod.orange)
 
     monster.char = '%'
     monster.color = libtcod.dark_red
